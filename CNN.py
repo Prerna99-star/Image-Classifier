0cc11f93bc3ef4c24b5242798cdfb98192f82c59
Prerna Mishra <59911271+Prerna99-star@users.noreply.github.com>
2020-05-13 11:55:54 +0530

    CNN
    .

---
+++ CNN.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 11:53:38 2020
+
+Convolutional Neural Network 
+"""
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers.core import Dense
+
+# Initialising tha CNN
+classifier= Sequential()
+
+# Step 1: Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2: Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3: Flattening
+classifier.add(Flatten())
+
+# Step 4: Full Connection
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the CNN to images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+ 
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                target_size=(64, 64),
+                                                batch_size=32,
+                                                class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                             target_size=(64, 64),
+                                             batch_size=32,
+                                             class_mode='binary')
+
+classifier.fit(training_set,
+               steps_per_epoch=8000,
+               epochs=25,
+               validation_data=test_set,
+               validation_steps=2000)
