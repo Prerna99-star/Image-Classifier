e18eda74a9323f3d99f484cc5c7487ed4218a83c
Prerna Mishra <mishraprerna218@gmail.com>
2020-05-23 12:24:03 +0530

    Update CNN.py
    given image is cat or dog

--- CNN.py
+++ CNN.py
@@ -59,3 +59,15 @@ classifier.fit(training_set,
                epochs=25,
                validation_data=test_set,
                validation_steps=2000)
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
